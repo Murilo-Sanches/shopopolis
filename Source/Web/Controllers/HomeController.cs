0abac5b688212a17c4a7fcdca4d9eee6f3390649
Murilo-Sanches <murilo.sanches.9999@outlook.com>
2024-03-21 13:39:00 -0300

    refactor: change namespaces

--- Source/Web/Controllers/HomeController.cs
+++ Source/Web/Controllers/HomeController.cs
@@ -1,8 +1,8 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Shopopolis.Models;
+using Shopopolis.Source.Core.Models;
 
-namespace Shopopolis.Controllers;
+namespace Shopopolis.Source.Web.Controllers;
 
 public class HomeController : Controller
 {
